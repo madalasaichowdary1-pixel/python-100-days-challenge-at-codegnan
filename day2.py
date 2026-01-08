6fe1d965a5dc56a1798c930a50a4da6889b7c8ed
sai chowdary <madalasaichowdary1@gmail.com>
2026-01-08 16:59:57 +0530

    Add files via upload
    day2 at codegnan

---
+++ day2.py
@@ -0,0 +1,18 @@
+num=10
+Decimal_value=20
+Name="sai praneeth"  # string type
+Age=22
+list_1=[1,2,3,4,5,"Hi",2,4,(10,20)] # list of items
+tuple=(10,20,30,"welcome",'hello') # Tuple of items
+cards={'A',2,3,4,5,6,7,8,9,10,'J','Q','K',,2,3,4} # pack of cards in set datatype
+student_marks = ("maths:90","telugu":75,"english":75) # dictionary datatype
+
+
+
+print("My name is:",name)
+print("My age is",age)
+print("My weight is:",weight)
+print("list of items",list1)
+print("Tuple of items",tuple1)
+print("pack of cards value",card1)
+print("dictionary fatatype value",dict1)
