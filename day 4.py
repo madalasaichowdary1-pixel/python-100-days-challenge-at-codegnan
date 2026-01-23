789c77398de7f362e5e8bcf590dae38bfbbb6b14
sai chowdary <madalasaichowdary1@gmail.com>
2026-01-23 16:19:48 +0530

    Add files via upload

---
+++ day 4.py
@@ -0,0 +1,43 @@
+
+"""# if number is zero print like "Enter number is zero"
+
+n == int(integer())
+if n==0:
+    print("entered number is zero")
+    print ("progam done")
+
+
+
+
+
+# check number is zero or not
+if num == 0:
+    print("number is not zero")
+    
+else:
+    print("number is zero")
+
+    
+#approch2
+num = int(input("enter a numbeer"))   
+if num != 0:
+    print("number is not zero")
+else:
+    prient("number is not zero")
+
+#3 to check even or odd
+    numint(input
+"""
+num=int(input())
+if num == O:
+    print("the number is ZERO")
+else:
+    print("the number is not ZERO")
+
+ #4
+num=int(input())
+if num>=10 and num%2==0:
+    print("the numberis even")
+even:
+    print("the number is odd")
+    
