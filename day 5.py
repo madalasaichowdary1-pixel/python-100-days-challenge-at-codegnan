113319fc46b53ba99ec3f53a8b8472d84df23c3e
sai chowdary <madalasaichowdary1@gmail.com>
2026-01-23 16:20:26 +0530

    Add files via upload

---
+++ day 5.py
@@ -0,0 +1,49 @@
+n =int("input"())
+
+if n % 2 == 0:
+     if n > -1:
+         print("number is +ve even")
+
+     else:
+        print("number is -ve even")
+
+lse:
+    if n > -1:
+        print("number is =ve odd")
+    else:
+        print("number is -ve odd")
+
+
+
+
+# check witch one even number or odd numberr in the given list of number
+
+"""lst = [2,4,3,5,6,7,9]:
+
+for num in 1st:    
+    if num% 2==0:
+        print(*num, "is even")
+    else:
+        print(num, "is odd")
+print("program done")"""
+
+# check witch one positive number or onegative numberr in the given list of number
+"""lst = [2,4,-3,-4,-6,-7,-9]:
+
+for num in 1st:    
+    if num>=0:
+        print(*num, "is positive")
+    else:
+        print(num, "is negative")
+print("program done")
+# print 1 - 20 number using range function
+
+for num range (1,21,1):
+ print (num)
+
+
+
+#print even number in between 0-20 using range fuction
+
+for num in range (21,2):
+print(num, end = "")
